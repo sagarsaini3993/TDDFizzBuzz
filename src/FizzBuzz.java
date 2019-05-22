49e903a7dbb0dfde0d29bedcc6eddfeee74ce797
gifty <gifty@DESKTOP-POJGJUA>
2019-05-22 12:23:30 -0400

    R3-Green

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,9 +4,14 @@ public class FizzBuzz {
 	
 	public String buzz(int num) {
 		
-		if (num % 5 == 0) {
+		if (num % 5 == 0 && num % 3 == 0) {
+			return "fizzbuzz";
+		}
+		
+		else if (num % 5 == 0) {
 			return "buzz";
 		}
+		 
 		return "fizz";
 	}
 
